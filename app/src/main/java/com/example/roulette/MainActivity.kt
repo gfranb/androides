b085d938cdf5f37740577886669eb09ebea071cf
gfranb <103999749+gfranb@users.noreply.github.com>
2023-03-10 19:23:56 +0100

    Comiteo proyecto

---
+++ app/src/main/java/com/example/roulette/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.roulette
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.example.roulette.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    private val m0nNavMenu = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when(item.itemId){
+            R.id.jugar -> {
+                supportFragmentManager.commit{
+                    replace<GameFragment>(R.id.frameLayout)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.clasificacion -> {
+                supportFragmentManager.commit{
+                    replace<ClasificacionFragment>(R.id.frameLayout)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.commit{
+            replace<GameFragment>(R.id.frameLayout)
+            setReorderingAllowed(true)
+            addToBackStack("replacement")
+        }
+
+        binding.bottomNavigationView.setOnItemSelectedListener(m0nNavMenu)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/roulette/StartScreen.kt
@@ -0,0 +1,26 @@
+package com.example.roulette
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.roulette.databinding.ActivityStartScreenBinding
+
+class StartScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Hago el binding y lo muestro e inflo la vista.
+        binding = ActivityStartScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener(){
+            val context = binding.btnStart.context
+            val intent = Intent(context, MainActivity::class.java)
+            context.startActivity(intent)
+        }
+
+    }
+}
