82db887d554d60ac87196ae68500277216eb316d
mparragarci <mparragarci@uoc.edu>
2023-05-05 16:24:50 +0200

    Producto2 finalizado

--- app/src/main/java/com/example/roulette/ApuestaDB.kt
+++ app/src/main/java/com/example/roulette/ApuestaDB.kt
@@ -1,13 +1,10 @@
 package com.example.roulette
 
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 
 @Database(entities = [Apuesta::class], version = 1)
-abstract class ApuestaDB: RoomDatabase() {
+abstract class ApuestaDB : RoomDatabase() {
 
     abstract fun apuestaDao(): ApuestaDao
 
